fb365ed1f0e1ff9bc884e40c8d2b1541668e422d
sritar99 <sadhutarun00@gmail.com>
2020-02-22 21:39:14 +0530

    upto Adding of  Navbar button (Add)

--- Todo/AppDelegate.swift
+++ Todo/AppDelegate.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
+@available(iOS 13.0, *)
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let items = ["Praneetha","Prathyusha","Harsha","Tarun"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
 
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        
+        let alert  = UIAlertController(title: "Add New ToDo", message: "", preferredStyle: .alert)
+//        Button Name is Add ToDo
+        let action = UIAlertAction(title: "Add ToDo", style: .default) { (action) in
+//            what will be happen once the user clicks the Add ToDo button on our Alert
+            
+        }
+        alert.addAction(action)
+    }
+    
 
 }
 
